e9b335c68c8863647b4187af1096a6a67b9f5c70
Lawrence Han <lawrench@andrew.cmu.edu>
2018-08-03 17:07:54 -0400

    introduced model select and tidied format up

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,5 +1,14 @@
-generate_data = function(n,p) {
+#2a
+
+generate_data <- function(n,p) {
   covariates <-(matrix(runif(n*p), n, p))
   responses <- (as.vector(runif(n)))
   list(covariates,responses)
 }
+
+#2b
+
+model_select <- function(covariates, responses, cutoff) {
+  lm(responses[which(covariates <= cutoff)]~covariates[which(covariates <= cutoff)])
+}
+
